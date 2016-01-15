664374f8ab9be36145ba950a05acd45cbecb6ab7
j-rod <j-rod@149-160-156-15.dhcp-bl.indiana.edu>
2016-01-15 12:25:31 -0500

    This is the second version.

--- lab1/src/RandomDNA.java
+++ lab1/src/RandomDNA.java
@@ -1,6 +1,29 @@
-
-public class HelloWorld {
-	public static void main(String[] args){
-		System.out.println("hello");
+import java.util.Random;
+public class RandomDNA {
+	
+	String produce(){      //this will produce a random DNA String of length 20
+		String s = "";
+		for (int i = 1; i<=20 ; i++){
+			Random rnd = new Random();
+			int t = rnd.nextInt(4);
+			if (t==0){
+				s = s +"A";
+			}
+			if (t==1){
+				s = s + "T";
+			}
+			if (t==2){
+				s = s + "C";
+			}
+			if (t==3){
+				s = s + "G";
+			}
+		}
+		return s;
 	}
-}
+	
+	public static void main(String[] args){
+		RandomDNA dna = new RandomDNA();
+		System.out.println(dna.produce());
+		}
+	}
