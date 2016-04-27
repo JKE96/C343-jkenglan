eceae557d33ea494690b4cbdf92cfe9fe3d43178
Jarod England <jkenglan@indiana.edu>
2016-04-26 23:14:52 -0400

    first version

---
+++ hw12/src/BinNode.java
@@ -0,0 +1,34 @@
+// Binary node with multiple keys
+// Yuzhen Ye, C343, Spring 2016
+
+public class BinNode<Key, E> {
+	private Key[] key; //can be any dimension; multiple keys
+	private E value;
+	private BinNode<Key, E> left;
+	private BinNode<Key, E> right;
+	public BinNode(Key[] k, E e) {
+		key = k;
+		value = e;
+		left = right = null;
+	}
+	public Key[] getKey() { return key; }
+	public E getValue() { return value; }
+	public void setKey(Key[] k) { key = k; }
+	public void setValue(E e) { value = e; }
+	public void setLeft(BinNode<Key, E> node) { left = node; }
+	public void setRight(BinNode<Key, E> node) { right = node; }
+	public void removeLeft() { left = null; }
+	public void removeRight() { right = null; }
+	public BinNode<Key, E> getLeft() { return left; }
+	public BinNode<Key, E> getRight() { return right; }
+	public boolean isLeaf() {
+		if((left == null) && (right == null)) return true;
+		else return false;
+	}
+	public String toString() {
+		String tmp = "[";
+		for(int i = 0; i < key.length; i ++) tmp += key[i] + " ";
+		tmp += value + "]";
+		return tmp;
+	}
+}
---
+++ hw12/src/README.txt
@@ -0,0 +1,42 @@
+Jarod England C343
+
+1.
+
+Original:           37
+                V———    ———V
+               24          42
+           V———  ———V  V——— ————V
+           7       32  40       42
+       V———                       ———V
+       2                             120
+                                        ————V
+                                           150 (Inserting)
+
+Shift graph root:   
+                     40
+                V———    ————V
+                32          42
+           V———   ———V   V——— ————V
+           24       34   42       120 
+       V———                         ———V
+      7                                150
+   V———
+   2
+
+Rotate bottom left of tree:  
+                     40
+                V———    ———V
+                32         42
+           V———  ———V   V——— ————V
+           7       34  42       120 
+       V——— ——V                   ———V
+      2       24                     150
+
+
+2. Refer to image
+
+
+
+
+
+
